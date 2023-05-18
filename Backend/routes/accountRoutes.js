dcd62dae796098d7ccd6c93aa7e54fa5cd41ce15
kanayofirms <kanyeze@gmail.com>
2023-05-18 16:41:33 +0100

    Update account routes endpoint

--- Backend/routes/accountRoutes.js
+++ Backend/routes/accountRoutes.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import * as accountController from "../controllers/accountController.js";
 import validateRequestBody from "../middlewares/validateBody.js";
-import registerSchema from "../schemas/accountSchema.js";
+import registerSchema from "../schemas/accountSchema/registerSchema.js";
 
 const router = Router();
 
