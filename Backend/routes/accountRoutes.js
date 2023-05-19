8b7fca2c022fa5902e9ecea75bab26025b0ec6f9
kanayofirms <kanyeze@gmail.com>
2023-05-19 05:57:10 +0100

    Update endpoint for login service

--- Backend/routes/accountRoutes.js
+++ Backend/routes/accountRoutes.js
@@ -2,13 +2,13 @@ import { Router } from "express";
 import * as accountController from "../controllers/accountController.js";
 import validateRequestBody from "../middlewares/validateBody.js";
 import registerSchema from "../schemas/accountSchema/registerSchema.js";
+import loginSchema from "../schemas/accountSchema/loginSchema.js";
 
 const router = Router();
 
-router.post(
-    "/register",
-    validateRequestBody(registerSchema),
-    accountController.register
-)
+router.post("/register", validateRequestBody(registerSchema), accountController.register);
+
+router.post("/login", validateRequestBody(loginSchema), accountController.login);
+
 
 export default router;
