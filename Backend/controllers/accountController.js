170677458a9f21bd82bb8ef8b7ef0517ca978efa
kanayofirms <kanyeze@gmail.com>
2023-05-19 05:54:23 +0100

    Update account controller for login service

--- Backend/controllers/accountController.js
+++ Backend/controllers/accountController.js
@@ -1,7 +1,12 @@
+import loginService from "../services/accountService/loginService.js";
 import registerService from "../services/accountService/registerService.js";
 
 const register = async (req, res) => {
     await registerService(req, res);
 };
 
-export { register };
+const login = async (req, res) => {
+    await loginService(req, res);
+};
+
+export { register, login };
