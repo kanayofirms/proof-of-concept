e2344618c12b7ba159284f8ac88bfe09047a5d36
kanayofirms <kanyeze@gmail.com>
2023-05-24 13:41:11 +0100

    Import and edit react query devtools and query client

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -1,12 +1,17 @@
-import React from "react"
-import ReactDOM from "react-dom/client.js"
-import App from "./App.jsx"
-import "./styles/main.scss"
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./styles/main.scss";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { queryClient } from "./config/config.js";
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+      <QueryClientProvider client={queryClient}>
     <App />
+        <ReactQueryDevtools initialIsOpen />
+      </QueryClientProvider>
   </React.StrictMode>,
 )
