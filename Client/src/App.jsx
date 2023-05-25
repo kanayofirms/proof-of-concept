229b7d69222c77d8365e27e4ac9caacc8f95a8a8
kanayofirms <kanyeze@gmail.com>
2023-05-25 10:47:45 +0100

    Import and set global spinner if user is logged in

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -2,11 +2,14 @@ import { Fragment, useState } from 'react'
 import { createBrowserRouter, RouterProvider, Route, Link, Outlet, Navigate } from "react-router-dom";
 import "./styles/App.scss";
 import { useMyProfile } from './queries/authQueries';
+import GlobalSpinner from "./components/GlobalSpinner"
 
 function App() {
   const { data, isLoading } = useMyProfile();
   const router = createBrowserRouter([
-    { path: "login", element: <Fragment>Login</Fragment> },
+    { path: "login", element: <Fragment>
+      {!isLoading && data?.user?.id ? <Navigate to="/"/> : isLoading ? <GlobalSpinner/> : <h1>Login</h1>}
+    </Fragment> },
   ]);
     return (
   <div className='App'>
