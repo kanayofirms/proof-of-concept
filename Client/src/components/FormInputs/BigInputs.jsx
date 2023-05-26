27b2193b75cb6ad7c005ee9da69d7b72c3336602
kanayofirms <kanyeze@gmail.com>
2023-05-26 22:15:41 +0100

    Create component for form inputs

---
+++ Client/src/components/FormInputs/BigInputs.jsx
@@ -0,0 +1,44 @@
+//import React from "react";
+
+/*const BigInputs = ({ 
+    label, 
+    htmlFor, 
+    type, 
+    name, 
+    autocomplete, 
+    placeholder, 
+    value, 
+    onChange, 
+    err, 
+    register, 
+}) => {
+    return (
+    <dir className={StyleSheet.container}>
+        <label htmlFor={htmlFor}>{label}</label>
+        <div className={styles.inputWrapper}>
+            <input
+                type={type}
+                name={name}
+                autoComplete={autocomplete}
+                placeholder={placeholder}
+                onChange={onChange}
+                {...register}
+                />
+        </div>
+        <span className={styles.err}>{err}</span>
+    </dir>
+    );
+};
+
+LoginInput.defaultProps = {
+    label: "Email",
+    htmlFor: undefined,
+    type: undefined,
+    autocomplete: undefined,
+    placeholder: "Enter your Email",
+    value: "",
+    onChange: () => {},
+    err: "",
+};
+
+export default BigInputs; */
