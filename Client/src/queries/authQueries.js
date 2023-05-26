b8a5271a93f192acccc67c1ffbf64efff706a7fb
kanayofirms <kanyeze@gmail.com>
2023-05-26 21:51:57 +0100

    Create login middleware and update authqueries

--- Client/src/queries/authQueries.js
+++ Client/src/queries/authQueries.js
@@ -1,6 +1,9 @@
 import { queryClient, axios } from "../config/config";
 import { useMutation, useQuery } from "@tanstack/react-query";
 
+
+//const ME_KEY = ["Me"];
+
 const myProfile = async () => {
     try {
         const { data } = await axios.get("/myProfile");
@@ -12,7 +15,7 @@ const myProfile = async () => {
 
 const useMyProfile = () => 
     useQuery({
-        queryKey: ["Me"],
+        queryKey: ME_KEY,
         staleTime: Infinity,
         retry: false,
         queryFn: myProfile,
@@ -21,4 +24,50 @@ const useMyProfile = () =>
         },
     });
 
-    export { useMyProfile };
+    const login = async (body) => {
+        const { data } = await axios.post("/login", body);
+        return data;
+    };
+/*
+    const useLogin = () => useMutation({
+        mutationFn: login,
+        onSuccess: (data) => {
+            queryClient.invalidateQueries(ME_KEY);
+            return data;
+        },
+    });
+
+    const logout = async () => {
+        const { data } = await axios.post("/logout");
+        return data;    
+    };
+
+    const useLogout = () => useMutation({
+        mutationFn: logout,
+        onSuccess: (data) => {
+            queryClient.setQueriesData(ME_KEY, { authed: false });
+            return data;
+        },
+        onError: (error) => {
+            queryClient.setQueriesData(ME_KEY, { authed: false })
+        }
+    });
+
+    const register = async (body) => {
+        try{
+            const { data } = await axios.post("/register", body);
+            return data;
+        }catch (e){
+            throw e?.response?.data?.message;
+        }
+    };
+
+    const useRegister = () => useMutation({
+        mutationFn: register,
+        onSuccess: (data) => {
+            queryClient.invalidateQueries(ME_KEY);
+            return data;
+        },
+    });
+*/
+    export { useMyProfile, /*useLogin, useLogout, useRegister*/ };
