cde170c01f8bb0a0606e80d1d05b806ac74879a9
kanayofirms <kanyeze@gmail.com>
2023-05-27 15:26:36 +0100

    Import ThemeProvider and wrap app with ThemeProvider

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -5,12 +5,14 @@ import "./styles/main.scss";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { queryClient } from "./config/config.js";
-
+import { ThemeProvider } from "./context/ThemeProvider";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
       <QueryClientProvider client={queryClient}>
-    <App />
+        <ThemeProvider>
+          <App />
+        </ThemeProvider>
         <ReactQueryDevtools initialIsOpen />
       </QueryClientProvider>
   </React.StrictMode>,
