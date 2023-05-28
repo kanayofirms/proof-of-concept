f3a547a104f9d4ea518fc4e144fefcdc74bb7042
kanayofirms <kanyeze@gmail.com>
2023-05-28 05:21:56 +0100

    Import Button components to Login

--- Client/src/pages/Login/Login.jsx
+++ Client/src/pages/Login/Login.jsx
@@ -1,4 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
+import {link } from "react-router-dom";
+import { useLogin, useMyProfile } from "../../queries/authQueries";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import styles from "./Login.module.scss";
+import Input from "../../components/FormInputs/BigInput";
+import Logo from "./Logo";
+import GlobalSpinner from "../../components/GlobalSpinner";
+import Button from "../../components/Button";
 
 const Login = () => {
     return <div>Login Page</div>;
