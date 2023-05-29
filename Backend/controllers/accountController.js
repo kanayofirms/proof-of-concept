79915cc852daac745ad4b0c38418ee65a02739eb
kanayofirms <kanyeze@gmail.com>
2023-05-29 23:59:56 +0100

    Fix: myprofile function and await myProfileService

--- Backend/controllers/accountController.js
+++ Backend/controllers/accountController.js
@@ -1,7 +1,7 @@
 import loginService from "../services/accountService/loginService.js";
 import registerService from "../services/accountService/registerService.js";
 import logoutService from "../services/accountService/logoutService.js";
-import myProfile from "../services/accountService/myProfileService.js";
+import myProfileService from "../services/accountService/myProfileService.js";
 
 const register = async (req, res) => {
     await registerService(req, res);
@@ -16,7 +16,7 @@ const logout = async (req, res) => {
 };
 
 const myProfile = async (req, res) => {
-    await myProfile(req, res);
+    await myProfileService(req, res);
 };
 
 export { register, login, logout, myProfile };
