7b4cef13f6fce74c1a3839c9bc71aea0b723033c
kanayofirms <kanyeze@gmail.com>
2023-05-31 04:58:11 +0100

    Refactor login module to fetch login page

--- Client/src/pages/Login/Login.jsx
+++ Client/src/pages/Login/Login.jsx
@@ -1,5 +1,6 @@
+
 import React, { useState } from "react";
-import {link } from "react-router-dom";
+/*import { link } from "react-router-dom";
 import { useLogin, useMyProfile } from "../../queries/authQueries";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -8,9 +9,24 @@ import Input from "../../components/FormInputs/BigInput";
 import Logo from "./Logo";
 import GlobalSpinner from "../../components/GlobalSpinner";
 import Button from "../../components/Button";
-
+import loginSchema from "../../schemas/loginSchema";
+*/
 const Login = () => {
+    return <div>Login Page</div>
+        };
+
+/*
+    const { mutate: login, isLoading, isError, error } = useLogin();
+    const {
+        data: me,
+        isLoading: meLoading,
+        isRefetching: meRefetching,
+    } = useMyProfile();
+
+    const { register, handleSubmit, reset, formState: { errors } } = useForm({
+        resolver: zodResolver(loginSchema),
+    });
     return <div>Login Page</div>;
-};
+};*/
 
 export default Login;
