849155ee69a7a7e3cd86ab99445fa16801b627b4
kanayofirms <kanyeze@gmail.com>
2023-05-31 23:54:52 +0100

    Feat: refactor app function for functionality and readability

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,37 +1,36 @@
-import { Fragment, useState } from 'react'
+import { Fragment, useState } from "react"
 import { createBrowserRouter, RouterProvider, Route, Link, Outlet, Navigate } from "react-router-dom";
 import "./styles/App.scss";
-import { useMyProfile } from './queries/authQueries';
-import GlobalSpinner from "./components/GlobalSpinner"
+import { useMyProfile } from "./queries/authQueries";
+import GlobalSpinner from "./components/GlobalSpinner";
 import Login from "./pages/Login"; 
-import { useTheme } from "./context/ThemeProvider"
+//import { useTheme } from "./context/ThemeProvider"
 
 function App() {
   const { data, isLoading } = useMyProfile();
-  const { mode } = useTheme();
+  //const { mode } = useTheme();
   const router = createBrowserRouter([
-    {
-      path: "login",
-      element: (
-        <Fragment>
-          {!isLoading && data?.user?.id ? (
-            <Navigate to="/" />
-          ) : isLoading ? (
-            <GlobalSpinner />
-          ) : (
-            <Login />
-          )}
-        </Fragment>
-      ),
-    },
-    {
-      path: "/",
-      element: (
-        <Fragment>
-          {isLoading ? (
-            <GlobalSpinner />
-          ) : data && !data?.authed ? (
-            <Navigate to="/login" />
+    { path: "login", 
+    element: (
+    <Fragment>
+      {!isLoading && data?.id ? (
+        <Navigate to="/" />
+        ) : isLoading ? (
+        <GlobalSpinner/>
+        ) : (
+        <Login />
+        )}
+    </Fragment> 
+  ),
+},
+{ 
+  path: "/", 
+  element: (
+    <Fragment>
+      {isLoading ? (
+        <GlobalSpinner />
+        ) : data && !data?.authed ? (
+          <Navigate to="/login" />
           ) : (
             <h1>Hello user</h1>
           )}
@@ -41,11 +40,10 @@ function App() {
   ]);
 
     return (
-  <div className={`App ${mode && "darkMode"}`}>
-    <RouterProvider router={router} />
-  </div>
-  
-  );
+      <div className='App'>
+      <RouterProvider router={router} />
+    </div>
+    );
 }
 
 export default App;
