805edddf94b5740a9781ab35e22c13003a56998f
kanayofirms <kanyeze@gmail.com>
2023-05-31 23:26:05 +0100

    Edit: remove global spinner wrappers and refactor code

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -5,15 +5,13 @@ import "./styles/main.scss";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { queryClient } from "./config/config.js";
-import { ThemeProvider } from "./context/ThemeProvider";
+//import { ThemeProvider } from "./context/ThemeProvider";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
       <QueryClientProvider client={queryClient}>
-        <ThemeProvider>
-          <App />
-        </ThemeProvider>
-        <ReactQueryDevtools initialIsOpen />
+        <App />
+          <ReactQueryDevtools initialIsOpen />
       </QueryClientProvider>
   </React.StrictMode>,
 )
