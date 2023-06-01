c72e31988cb988dc627da20a4481dc06652f570d
kanayofirms <kanyeze@gmail.com>
2023-06-01 08:43:04 +0100

    Fix: check for misspellings and wrap app with themeprovider

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -5,13 +5,15 @@ import "./styles/main.scss";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { queryClient } from "./config/config.js";
-//import { ThemeProvider } from "./context/ThemeProvider";
+import { ThemeProvider } from "./context/ThemeProvider";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
       <QueryClientProvider client={queryClient}>
-        <App />
-          <ReactQueryDevtools initialIsOpen />
+        <ThemeProvider>
+          <App />
+        </ThemeProvider>
+        <ReactQueryDevtools initialIsOpen />
       </QueryClientProvider>
   </React.StrictMode>,
 )
